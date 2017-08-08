88e4ff5f1aae7c8b2ce58f4b9afb7edfec641fa4
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-08-08 11:15:30 -0700

    [Finishes #149598317] Fixes form formatting
    We also changed the indentation so that it was just two spaces.

--- client/containers/names.jsx
+++ client/containers/names.jsx
@@ -1,70 +1,89 @@
 'use strict';
 
 import React, {PropTypes} from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
 
-import { submitNames } from "../actions/index";
+import {submitNames} from "../actions/index";
 
 class Names extends React.Component {
-    constructor(props, context) {        
-        super(props, context);
-        
-        props.names ? this.state = { names: props.names } : this.state = { names : { first: "", middle: "", last: "" } };
+  constructor(props, context) {
+    super(props, context);
 
-        this.onChangeNames = this.onChangeNames.bind(this);
-        this.onSubmitNames = this.onSubmitNames.bind(this);
-    }
-
-    onChangeNames(event) {
-        const names = this.state.names;
-        switch(event.target.name){
-            case "firstName":
-                names.first = event.target.value;
-                break;
-            case "middleName":
-                names.middle = event.target.value;
-                break;
-            case "lastName":
-                names.last = event.target.value;
-                break;
-            default:
-                break;
+    props.names
+      ? this.state = {
+        names: props.names
+      }
+      : this.state = {
+        names: {
+          first: "",
+          middle: "",
+          last: ""
         }
-        //Update component state.
-        this.setState( { names: names } );
-    }
+      };
 
-    onSubmitNames(event) {
-        event.preventDefault();
-        this.props.submitNames(this.state.names);
-    }
+    this.onChangeNames = this.onChangeNames.bind(this);
+    this.onSubmitNames = this.onSubmitNames.bind(this);
+  }
 
-    render() {        
-        return (
-            <div>
-                <Link to='/' >Back to application</Link>
-                <form onSubmit={this.onSubmitNames} className="input-container">
-                    <input type="text" name="firstName" placeholder="First Name" onChange={this.onChangeNames} value={this.state.names.first} />                    
-                    <input type="text" name="middleName" placeholder="Middle Name" onChange={this.onChangeNames} value={this.state.names.middle} />
-                    <input type="text" name="lastName" placeholder="Last Name" onChange={this.onChangeNames} value={this.state.names.last} />
-                    <button type="submit" name="submitNamesButton"> Submit </button>
-                </form>
-            </div>
-        )
+  onChangeNames(event) {
+    const names = this.state.names;
+    switch (event.target.name) {
+      case "firstName":
+        names.first = event.target.value;
+        break;
+      case "middleName":
+        names.middle = event.target.value;
+        break;
+      case "lastName":
+        names.last = event.target.value;
+        break;
+      default:
+        break;
     }
+    //Update component state.
+    this.setState({names: names});
+  }
+
+  onSubmitNames(event) {
+    event.preventDefault();
+    this.props.submitNames(this.state.names);
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to='/'>Back to application</Link>
+        <form onSubmit={this.onSubmitNames}>
+          <label htmlFor="firstName">First Name</label>
+          <div className="input-container">
+            <input type="text" id="firstName" name="firstName" placeholder="First Name" onChange={this.onChangeNames} value={this.state.names.first}/>
+          </div>
+          <label htmlFor="middleName">Middle Name</label>
+          <div className="input-container">
+            <input type="text" id="middleName" name="middleName" placeholder="Middle Name" onChange={this.onChangeNames} value={this.state.names.middle}/>
+          </div>
+          <label htmlFor="lastName">Last Name</label>
+          <div className="input-container">
+            <input type="text" id="lastName" name="lastName" placeholder="Last Name" onChange={this.onChangeNames} value={this.state.names.last}/>
+          </div>
+          <input type="submit" name="submitNamesButton" value="Submit" />
+        </form>
+      </div>
+    )
+  }
 
 }
 
-function mapStateToProps(state) {  
-  return {
-    names: state.names
-  };
+function mapStateToProps(state) {
+  return {names: state.names};
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ submitNames: submitNames }, dispatch);
+  return bindActionCreators({
+    submitNames: submitNames
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Names);
