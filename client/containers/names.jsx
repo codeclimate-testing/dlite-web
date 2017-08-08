ecd25220d35e3413aeaeb7095c91b552e06b1681
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-08-08 12:13:10 -0700

    Extracting name form presentation into separate object

--- client/containers/names.jsx
+++ client/containers/names.jsx
@@ -7,6 +7,29 @@ import {bindActionCreators} from "redux";
 
 import {submitNames} from "../actions/index";
 
+const LegalNameForm = (props) => {
+  return (
+    <div className='legal-name-form'>
+      <Link to='/'>Back to application</Link>
+      <form onSubmit={props.onSubmitNames}>
+        <label htmlFor="firstName">First Name</label>
+        <div className="input-container">
+          <input type="text" id="firstName" name="firstName" placeholder="First Name" onChange={props.onChangeNames} value={props.state.names.first}/>
+        </div>
+        <label htmlFor="middleName">Middle Name</label>
+        <div className="input-container">
+          <input type="text" id="middleName" name="middleName" placeholder="Middle Name" onChange={props.onChangeNames} value={props.state.names.middle}/>
+        </div>
+        <label htmlFor="lastName">Last Name</label>
+        <div className="input-container">
+          <input type="text" id="lastName" name="lastName" placeholder="Last Name" onChange={props.onChangeNames} value={props.state.names.last}/>
+        </div>
+        <input type="submit" name="submitNamesButton" value="Submit" />
+      </form>
+    </div>
+  )
+};
+
 class Names extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -53,24 +76,7 @@ class Names extends React.Component {
 
   render() {
     return (
-      <div>
-        <Link to='/'>Back to application</Link>
-        <form onSubmit={this.onSubmitNames}>
-          <label htmlFor="firstName">First Name</label>
-          <div className="input-container">
-            <input type="text" id="firstName" name="firstName" placeholder="First Name" onChange={this.onChangeNames} value={this.state.names.first}/>
-          </div>
-          <label htmlFor="middleName">Middle Name</label>
-          <div className="input-container">
-            <input type="text" id="middleName" name="middleName" placeholder="Middle Name" onChange={this.onChangeNames} value={this.state.names.middle}/>
-          </div>
-          <label htmlFor="lastName">Last Name</label>
-          <div className="input-container">
-            <input type="text" id="lastName" name="lastName" placeholder="Last Name" onChange={this.onChangeNames} value={this.state.names.last}/>
-          </div>
-          <input type="submit" name="submitNamesButton" value="Submit" />
-        </form>
-      </div>
+      <LegalNameForm onSubmitNames={this.onSubmitNames} onChangeNames={this.onChangeNames} state={this.state} />
     )
   }
 
