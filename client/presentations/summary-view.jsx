9dd5cce912ae95860e1edf7ddfefe99c60b655c0
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-08-08 16:57:46 -0700

    Reloads on client paths work:
            * Whole client app in the `/services` namespace
            * Fixes bug in layout where the app loading is relative
            * Creates client side helper for putting alice in its namespace

--- client/presentations/summary-view.jsx
+++ client/presentations/summary-view.jsx
@@ -3,10 +3,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+import alicePath from '../helpers/alice-path';
+
 export const SummaryEmpty = (props) => {
   return (
     <div className='summary-page'>
-      <Link to='/' >Back to application</Link>
+      <Link to={ alicePath('/') } >Back to application</Link>
       <p>No information has been entered yet</p>
     </div>
   );
@@ -15,7 +17,7 @@ export const SummaryEmpty = (props) => {
 export const SummaryNames = (props) => {
   return (
     <div className='summary-page'>
-      <Link to='/' >Back to application</Link>
+      <Link to={ alicePath('/') } >Back to application</Link>
       <p> First Name: {props.legalName.firstName} </p>
       <p> Middle Name: {props.legalName.middleName} </p>
       <p> Last Name: {props.legalName.lastName} </p>
