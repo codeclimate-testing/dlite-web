b7b426e635198b92893227123aa6303aeadb1fd1
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-08-14 11:36:39 -0700

    Refactors legal name form container
    This extracts some logic that can help us hook up forms rapidly

---
+++ client/containers/connect-form.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import { connect } from "react-redux";
+
+import onInputChange from './on-input-change';
+import onSubmit      from './on-form-submit';
+
+function connectForm(mapStateToProps, action, form) {
+  function mapDispatchToProps(dispatch) {
+    const onChange = onInputChange(action, dispatch);
+
+    return {
+      onSubmit,
+      onChange
+    };
+  }
+
+  return connect(mapStateToProps, mapDispatchToProps)(form);
+};
+
+export default connectForm;
--- client/containers/legal-name.jsx
+++ client/containers/legal-name.jsx
@@ -1,41 +1,24 @@
 'use strict';
 
-import React, {PropTypes} from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from "react-redux";
-// import {bindActionCreators} from "redux";
+import React from 'react';
 
-import {updateLegalName} from "../actions/index";
-import LegalNameForm from "../presentations/name-form.jsx";
+import { updateLegalName }  from "../actions/index";
+import LegalNameForm        from "../presentations/name-form.jsx";
+import connectForm          from './connect-form';
 
 const LegalName = (props) => {
   return (
-    <LegalNameForm onSubmit={props.onSubmit} onChange={props.onChange} legalName={props.legalName} />
+    <LegalNameForm
+      onSubmit={props.onSubmit}
+      onChange={props.onChange}
+      legalName={props.legalName} />
   );
 };
 
 function mapStateToProps(state) {
-  return {legalName: state.legalName};
-}
-
-function mapDispatchToProps(dispatch) {
-  function onChange(event) {
-    let target  = event.target;
-    let nameType = target.getAttribute('name');
-    let value = target.value;
-    dispatch(updateLegalName(nameType, value));
-  }
-
-  function onSubmit(event) {
-    event.preventDefault()
-    // no-op, right now
-    // might change the route in the future
-  }
-
   return {
-    onSubmit,
-    onChange
+    legalName: state.legalName
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LegalName);
+export default connectForm(mapStateToProps, updateLegalName, LegalNameForm);
