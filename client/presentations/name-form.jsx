00f8bbb464e2ab7333648f9dcd10cf2aa0bf0e9c
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-08-18 08:36:44 -0700

    Cleans up NameForm
    Also fixes text input where type not given

--- client/presentations/name-form.jsx
+++ client/presentations/name-form.jsx
@@ -1,27 +1,36 @@
 'use strict';
 
 import React from 'react';
-import {Link} from 'react-router-dom';
 
-import alicePath from '../helpers/alice-path';
+import HomeLink  from './home-link.jsx';
+import TextInput from './text-input.jsx';
 
 const LegalNameForm = (props) => {
   return (
     <div className='legal-name-form'>
-      <Link to={ alicePath('/') }>Back to application</Link>
+      <HomeLink />
       <form onSubmit={props.onSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <div className="input-container">
-          <input type="text" id="firstName" name="firstName" placeholder="First Name" onChange={props.onChange} value={props.legalName.firstName}/>
-        </div>
-        <label htmlFor="middleName">Middle Name</label>
-        <div className="input-container">
-          <input type="text" id="middleName" name="middleName" placeholder="Middle Name" onChange={props.onChange} value={props.legalName.middleName}/>
-        </div>
-        <label htmlFor="lastName">Last Name</label>
-        <div className="input-container">
-          <input type="text" id="lastName" name="lastName" placeholder="Last Name" onChange={props.onChange} value={props.legalName.lastName}/>
-        </div>
+        <TextInput
+          identifier='firstName'
+          description='First name'
+          value={props.legalName.firstName}
+          onChange={props.onChange}
+        />
+
+        <TextInput
+          identifier='middleName'
+          description='Middle name'
+          value={props.legalName.middleName}
+          onChange={props.onChange}
+        />
+
+        <TextInput
+          identifier='lastName'
+          description='Last name'
+          value={props.legalName.lastName}
+          onChange={props.onChange}
+        />
+
         <input type="submit" name="submitNamesButton" value="Submit" />
       </form>
     </div>
