02ddeb54f3f5629950eb8119d49f798c0315e199
Randeep Singh <logicgraphy@gmail.com>
2017-08-18 11:07:11 -0700

    Fixes update of contact details with generateAction function

--- client/actions/index.js
+++ client/actions/index.js
@@ -13,12 +13,4 @@ export const updateLegalName        = generateAction('UPDATE_LEGAL_NAME');
 export const updateResidenceAddress = generateAction('UPDATE_RESIDENCE_ADDRESS');
 export const updateHairColor        = generateAction('UPDATE_HAIR_COLOR');
 export const updateEyeColor         = generateAction('UPDATE_EYE_COLOR');
-//export const updateContactDetails   = generateAction('UPDATE_CONTACT_DETAILS');
-
-// Not sure why this isn't working ^^^, but something about this is different ??
-export function updateContactDetails(type, value) {
-  return {
-    type:'UPDATE_CONTACT_DETAILS',
-    payload: { type, value }
-  }
-}
+export const updateContactDetails   = generateAction('UPDATE_CONTACT_DETAILS');
