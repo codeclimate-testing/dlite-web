070047ebdf39f2c4f7ba4897268ba5f025fc4cd9
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-09-05 14:37:39 -0700

    Summary related scenarios working

--- test/features/step_definitions/empty-summary-steps.js
+++ test/features/step_definitions/empty-summary-steps.js
@@ -6,21 +6,31 @@ module.exports = function(world) {
   let browser = world.browser;
 
   world.then('I should see a message \'No information has been entered yet\'', function(done) {
-    let text = browser.text('p');
-    assert(text.includes('No information has been entered yet'));
-    done();
+    browser
+      .text('p')
+      .then((text) => {
+        assert.ok(
+          text.includes('No information has been entered yet'),
+          'Empty summary message not found'
+        );
+      })
+      .then(() => { done(); })
+      .catch(done);
   });
 
   world.and('I will not see the link to the summary', function(done) {
-    let summaryUrl = world.url('/summary');
-    let links = browser.querySelectorAll(`a[href="${summaryUrl}"]`);
-    assert.equal(links.length, 0);
-    done();
+    browser
+      .exists('a.summary')
+      .then((exists) => { assert.ok(!exists, 'summary link present'); })
+      .then(() => { done(); })
+      .catch(done);
   });
 
   world.then('I should see a link to the summary', function(done) {
-    let links = browser.querySelectorAll('a');
-    assert.equal(links[0].href, world.url('/summary'));
-    done();
+    browser
+      .exists('a.summary')
+      .then((exists) => { assert.ok(exists, 'summary link not present'); })
+      .then(() => { done(); })
+      .catch(done);
   });
 };
