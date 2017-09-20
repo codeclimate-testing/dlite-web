3d0bbb964695acb1a5467d6c471017a60c0715e4
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-09-19 18:16:02 -0700

    Eye color form navigates to hair color form
            * Uses the new select buttons
            * Simplifies and concatenates the scenarios for faster run times
            * No new React unit tests, because all presentation and
              configuration of existing
            * Extracts helper function in navigation steps

--- test/features/step_definitions/eye-color-selection-steps.js
+++ test/features/step_definitions/eye-color-selection-steps.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 module.exports = function (world) {
   let browser = world.browser;
 
-  world.then('I will see buttons for Blue, Gray, Green, Hazel and Brown', function(done) {
+  world.then('I will see select buttons for Blue, Gray, Green, Hazel and Brown', function(done) {
     browser
       .html('label[for="Blue"]')
       .then((button) => { assert.ok(button, 'Selector for Blue eye color missing')})
@@ -21,23 +21,13 @@ module.exports = function (world) {
       .catch(done);
   });
 
-  world.given('I have already entered my eye color into the form', function(done){
+  world.given('I have already entered my eye color', function(done){
     browser
       .click('a.appearance-eye')
-      .click('label[for="Hazel"]')
+      .click('label[for="Blue"]')
       .click('input[type="submit"]')
       .click('a.home')
       .waitForSelector('.home-page')
-      .then(() => { done(); })
-      .catch(done);
-  });
-
-  world.then('I will see the eye color I selected', function(done) {
-    browser
-      .text()
-      .then((text) => {
-        assert.ok(text.includes('Blue'), 'Eye color not saved in summary');
-      })
       .then(() => { done(); })
       .catch(done);
   });
@@ -49,7 +39,7 @@ module.exports = function (world) {
       .catch(done);
   });
 
-  world.and('I see that eye color selected', function(done){
+  world.and('I will see the eye color I chose is selected', function(done){
     browser
       .text('.button.selected')
       .then((color) => { assert.equal(color, 'Blue'); })
@@ -57,41 +47,28 @@ module.exports = function (world) {
       .catch(done);
   });
 
-  world.and('I realize I made the wrong eye color selection and change it', function(done){
+  world.and('I change my eye color', function(done){
     browser
       .click('label[for="Brown"]')
       .then(() => { done(); })
       .catch(done);
   });
 
-  world.then('I will see the original eye color selection as not highlighted', function(done){
-    browser
-      .text('.button.selected')
-      .then((color) => { assert.ok(color !== 'Blue', 'Original color still selected'); })
-      .then(() => { done(); })
-      .catch(done);
-  });
-
-  world.and('I will see the new eye color selection has been highlighted', function(done){
-    browser
-      .text('.button.selected')
-      .then((color) => { assert.equal(color, 'Brown'); })
-      .then(() => { done(); })
-      .catch(done);
-  });
-
-  world.and('I change my eye color selection', function(done){
-    browser
-      .click('label[for="Gray"]')
-      .then(() => { done(); })
-      .catch(done);
-  });
-
-  world.then('I will see my updated eye color', function(done){
+  world.then('I will see my eye color in the summary', function(done){
     browser
       .text()
       .then((text) => {
-        assert.ok(text.includes('Gray'), 'Eye color not saved in summary');
+        assert.ok(text.includes('Blue'), 'Eye color not saved in summary');
+      })
+      .then(() => { done(); })
+      .catch(done);
+  });
+
+  world.then('I will see my updated eye color in the summary', function(done){
+    browser
+      .text()
+      .then((text) => {
+        assert.ok(text.includes('Brown'), 'Eye color not saved in summary');
       })
       .then(() => { done(); })
       .catch(done);
