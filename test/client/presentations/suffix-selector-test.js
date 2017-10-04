55b44ffaacd627ab5d7fc093b855b2e2a61aac87
Randeep Singh <logicgraphy@gmail.com>
2017-10-04 11:00:56 -0700

    Implements unit test for suffix selector

---
+++ test/client/presentations/suffix-selector-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import assert from 'assert';
+
+import 'jsdom-global/register';
+import React from 'react';
+import { render } from 'enzyme';
+import { spy } from 'sinon';
+
+import SuffixSelector from '../../../client/presentations/suffix-selector.jsx';
+
+describe('SuffixSelector', function() {
+  let component, onChange;
+
+  beforeEach(function() {
+    onChange = spy();
+
+    component = render(
+      <SuffixSelector
+        identifier='suffix'
+        value={ 'Sr.' }
+        onChange={ onChange }
+      />
+    )
+  });
+
+  it('has option to select each value', function() {
+    assert.equal(component.find('#suffix > option:nth-child(1)').text(), '');
+    assert.equal(component.find('#suffix > option:nth-child(2)').text(), 'Sr.');
+    assert.equal(component.find('#suffix > option:nth-child(3)').text(), 'Jr.');
+    assert.equal(component.find('#suffix > option:nth-child(4)').text(), 'II');
+    assert.equal(component.find('#suffix > option:nth-child(5)').text(), 'III');
+    assert.equal(component.find('#suffix > option:nth-child(6)').text(), 'IV');
+  });
+
+  it('has correct selected value', function() {
+    assert.equal(component.find('#suffix').val(), 'Sr.');
+  });
+
+});
