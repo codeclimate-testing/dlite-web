66c87ce00a18b393e581bf2377dfd584ef24ea1e
Deep Marreddy <smarreddy@cambriasolutions.com>
2017-10-12 13:34:12 -0700

    Update language on political party choose page

--- client/presentations/voter/political-party-choose-form.jsx
+++ client/presentations/voter/political-party-choose-form.jsx
@@ -5,7 +5,7 @@ import SelectorCollection      from '../selector-collection.jsx';
 import HomeLink                from '../home-link.jsx';
 import ContinueButton          from '../continue-button.jsx';
 
-const VALUES = ['Yes', 'No, I do not want to choose a political party'];
+const VALUES = ['Yes', 'I do not wish to choose a political party'];
 
 const PoliticalPartyChoose = (props) => {
   return (
@@ -13,7 +13,8 @@ const PoliticalPartyChoose = (props) => {
       <HomeLink />
 
       <h4>Would you like to choose a political party preference?</h4>
-      <p>You must be registered with a party to vote in its primary elections.</p>
+      <p>In order to vote for a presidential candidate in a primary election, you
+      may need to be registered with that political party.</p>
 
         <form onSubmit={ props.onSubmit } className='political-party-choose'>
         <div className='inner-bottom'>
--- test/features/step_definitions/political-party-choose-steps.js
+++ test/features/step_definitions/political-party-choose-steps.js
@@ -7,7 +7,7 @@ module.exports = function(world) {
 
   world.when('I select no political party', function(done){
     browser
-    .click('label[for="No, I do not want to choose a political party"]')
+    .click('label[for="I do not wish to choose a political party"]')
     .then(() => { done(); })
     .catch(done);
   });
@@ -26,7 +26,7 @@ module.exports = function(world) {
     browser
     .text()
     .then((text) => {
-      assert.ok(text.includes('No, I do not want to choose a political party'), 'No, I do not want to choose a political party not saved in summary');
+      assert.ok(text.includes('I do not wish to choose a political party'), 'I do not wish to choose a political party not saved in summary');
     })
     .then(() => { done(); })
     .catch(done);
