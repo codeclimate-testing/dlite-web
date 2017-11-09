5f23e57dc8b323b8abd2243f933a66dbbd7a6b3a
Randeep Singh <logicgraphy@gmail.com>
2017-11-08 16:58:11 -0800

    Fixes indentation for new files

--- client/actions/api-actions.js
+++ client/actions/api-actions.js
@@ -5,37 +5,37 @@ import axios from 'axios';
 //Required for running tests with node horseman
 require('es6-promise').polyfill();
 
-export const getData = function(apiHost, id) {
-	let url = apiHost + '/' + 'application' + '/' + id;
-	return function(dispatch) {
-		return axios({
-			url: url,
-			method: 'get',
-			responseType: 'json'
-		})
-			.then(function(response) {
-				dispatch({ type: 'GET_DATA_SUCCESS', data: response.data});
-			})
-			.catch(function(err){
-				dispatch({type: 'GET_DATA_ERROR', error: err.message, data: {}});
-			})
-	}
+export const getData = function (apiHost, id) {
+  let url = apiHost + '/' + 'application' + '/' + id;
+  return function (dispatch) {
+    return axios({
+      url: url,
+      method: 'get',
+      responseType: 'json'
+    })
+      .then(function (response) {
+        dispatch({ type: 'GET_DATA_SUCCESS', data: response.data });
+      })
+      .catch(function (err) {
+        dispatch({ type: 'GET_DATA_ERROR', error: err.message, data: {} });
+      })
+  }
 };
 
-export const postData =	function(apiHost, body) {
-	let url = apiHost + '/' + 'application';
-	return function(dispatch) {
-		return axios({
-			url: url,
-			data: body,
-			method: 'post',
-			responseType: 'json'
-		})
-			.then(function(response) {
-				dispatch({ type: 'POST_DATA_SUCCESS', data: response.data});
-			})
-			.catch(function(err){
-				dispatch({type: 'POST_DATA_ERROR', error: err.message, data: {}});
-			})
-	}
+export const postData = function (apiHost, body) {
+  let url = apiHost + '/' + 'application';
+  return function (dispatch) {
+    return axios({
+      url: url,
+      data: body,
+      method: 'post',
+      responseType: 'json'
+    })
+      .then(function (response) {
+        dispatch({ type: 'POST_DATA_SUCCESS', data: response.data });
+      })
+      .catch(function (err) {
+        dispatch({ type: 'POST_DATA_ERROR', error: err.message, data: {} });
+      })
+  }
 }
--- client/reducers/api-reducer.js
+++ client/reducers/api-reducer.js
@@ -1,18 +1,18 @@
 'use strict'
 
 const apiReducer = (state, action) => {
-	switch(action.type) {
+  switch (action.type) {
     case 'GET_DATA_SUCCESS':
       return Object.assign({}, state, action.data);
     case 'GET_DATA_ERROR':
-			return state;
+      return state;
     case 'POST_DATA_SUCCESS':
       return Object.assign({}, state, action.data);
     case 'POST_DATA_ERROR':
       return state;
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export default apiReducer;
