28caee22137faba4860aad6ea0d2f4fc468c63da
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-12-12 13:32:04 -0800

    Integrates the isPreregistering function and also fixes indentation and white space

--- client/containers/voter/ballot-by-mail-form-container.jsx
+++ client/containers/voter/ballot-by-mail-form-container.jsx
@@ -9,42 +9,41 @@ import connectForm                  from '../../helpers/connect-form';
 import * as dataPresent             from '../../helpers/data-present';
 import navigateOnSubmit             from '../../helpers/navigate-on-submit';
 import navigateOnBack               from '../../helpers/navigate-on-back';
-import { getCurrentAge }            from '../../helpers/calculate-age';
+import { isPreregistering }         from '../../helpers/calculate-age';
 
 const ConnectedForm = (props) => {
-
   const onSubmit = navigateOnSubmit('/voting-registration/contact-methods', props);
   const onBack = navigateOnBack('/voting-registration/language', props);
   let continueDisabled = !(dataPresent.value(props.ballotByMail));
   let content = [];
-  
-  if ((props.dateOfBirth.age >= 16 ) && (props.dateOfBirth.age <= 18)) {
-    content.push(
-    <BallotByMailFormPreReg 
-      key='Pre-registration Ballot by mail'
-      onSubmit          = {onSubmit}
-      onBack            = {onBack}
-      onChange          = {props.onChange}
-      selectedValue     = {props.ballotByMail}
-      continueDisabled  = {continueDisabled}
-      age               = {props.dateOfBirth.age}
-    />
-  );
-} 
-else {
-    content.push(
-    <BallotByMailForm 
-      key='Ballot by mail'
-      onSubmit          = {onSubmit}
-      onBack            = {onBack}
-      onChange          = {props.onChange}
-      selectedValue     = {props.ballotByMail}
-      continueDisabled  = {continueDisabled}
-      age               = {props.dateOfBirth.age}
-    />
-  );
-}
- return (
+
+  if (isPreregistering(props.dateOfBirth)) {
+      content.push(
+      <BallotByMailFormPreReg
+        key='Pre-registration Ballot by mail'
+        onSubmit          = {onSubmit}
+        onBack            = {onBack}
+        onChange          = {props.onChange}
+        selectedValue     = {props.ballotByMail}
+        continueDisabled  = {continueDisabled}
+        age               = {props.dateOfBirth.age}
+      />
+    );
+  } else {
+      content.push(
+      <BallotByMailForm
+        key='Ballot by mail'
+        onSubmit          = {onSubmit}
+        onBack            = {onBack}
+        onChange          = {props.onChange}
+        selectedValue     = {props.ballotByMail}
+        continueDisabled  = {continueDisabled}
+        age               = {props.dateOfBirth.age}
+      />
+    );
+  }
+
+  return (
     <div> {content} </div>
   );
 };
