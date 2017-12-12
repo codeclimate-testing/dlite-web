740d5792b3f8c2727bdd8f7bbd45f2e3d6b5d04e
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-12-12 15:47:48 -0800

    The unfinished work

--- client/presentations/voter/opt-out-form.jsx
+++ client/presentations/voter/opt-out-form.jsx
@@ -1,44 +1,12 @@
 'use strict';
 
 import React from 'react';
-import NavigationButtons from '../navigation-buttons.jsx';
-import Page from '../page.jsx';
-
-import RadioSelectorCollection from '../radio-selector-collection.jsx';
+import OptOutRadioPage from './opt-out-radio-page.jsx';
 import RadioSelector           from '../radio-selector.jsx';
-
-const OptOutRadioFormContainer = (props) => {
-  return (
-    <Page
-      {...props}
-      pageTitle={props.pageTitle}
-      sectionNumber='3'
-      sectionName={props.sectionName}
-    >
-      <div>
-        <h4>Which best describes you?</h4>
-        <form onSubmit={ props.onSubmit } className='opt-out-form'>
-          <RadioSelectorCollection
-            name='optOut'
-            onChange={props.onChange}
-            onBlur={props.onBlur}
-            onFocus={props.onFocus}
-            selectedValue={ props.selectedValue }
-            focused={props.focused}
-          >
-            {props.children}
-          </RadioSelectorCollection>
-
-          <NavigationButtons {...props} />
-        </form>
-      </div>
-    </Page>
-  );
-};
 
 const OptOutForm = (props) => {
   return (
-    <OptOutRadioFormContainer
+    <OptOutRadioPage
       pageTitle='DMV: License application - Voting registration'
       sectionName='Voting registration'
       {...props}
@@ -55,7 +23,7 @@ const OptOutForm = (props) => {
         value='opt-out'
         text='I am eligible to vote, but do not want to register to vote'
       />
-    </OptOutRadioFormContainer>
+    </OptOutRadioPage>
   );
 };
 
--- client/presentations/voter/opt-out-prereg-form.jsx
+++ client/presentations/voter/opt-out-prereg-form.jsx
@@ -1,43 +1,29 @@
 'use strict';
 
 import React from 'react';
-import RadioCollection    from '../radio-collection.jsx';
-
-import NavigationButtons from '../navigation-buttons.jsx';
-import Page from '../page.jsx';
-
-const VALUES = [
-  "I would like to pre-register to vote",
-  "I am already pre-registered to vote in California",
-  "I am eligible to vote, but do not want to pre-register to vote"
-];
+import OptOutRadioPage from './opt-out-radio-page.jsx';
+import RadioSelector           from '../radio-selector.jsx';
 
 const OptOutFormPreReg = (props) => {
-
   return (
-    <Page
+    <OptOutRadioPage
       pageTitle='DMV: License application - Voting pre-registration'
-      sectionNumber='3'
       sectionName='Voting pre-registration'
       {...props}
     >
-      <div>
-        <h4>Which best describes you?</h4>
-        <form onSubmit={props.onSubmit} className='opt-out-form'>
-          <div className='inner-bottom'>
-            <RadioCollection
-              key='radio'
-              name='optOut'
-              values={VALUES}
-              onChange={props.onChange}
-              selectedValue={props.selectedValue}
-            />
-          </div>
-
-          <NavigationButtons {...props} />
-        </form>
-      </div>
-    </Page>
+      <RadioSelector
+        value='new'
+        text='I would like to pre-register to vote'
+      />
+      <RadioSelector
+        value='existing'
+        text='I am already pre-registered to vote in California'
+      />
+      <RadioSelector
+        value='opt-out'
+        text='I am eligible to vote, but do not want to pre-register to vote'
+      />
+    </OptOutRadioPage>
   );
 };
 
