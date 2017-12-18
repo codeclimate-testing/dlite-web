1e589a48fdf719ed82358f44bec67ef1a222c45c
Kylie Pace <kpace@cambriasolutions.com>
2017-12-18 15:02:18 -0800

    rebased and changed file paths to updated locations

--- test/client/presentations/intro/choose-card-test.js
+++ test/client/presentations/intro/choose-card-test.js
@@ -7,7 +7,7 @@ import React          from 'react';
 import { render }     from 'enzyme';
 import sinon          from 'sinon';
 
-import ChooseCard     from '../../../../client/containers/apply/what-do-you-want-to-do-today-container.jsx';
+import ChooseCard     from '../../../../client/containers/intro/what-do-you-want-to-do-today-page.jsx';
 
 describe('Choose Card Form', function() {
   // next button disabled on render
--- test/features/enter-name.feature
+++
@@ -1,9 +0,0 @@
-
-  
-  Scenario: I want to go back to previous page
-    Given I go to the new online DL application page
-    When I visit the legal name page
-    # Then I will see a field for first, middle last name and suffix
-    When I click to go back
-    Then I will be on the get started page
->>>>>>> combined most navigation stories into one
