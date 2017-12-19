7639e4ff741a698c4fbd5502b135d1c9015d7fb8
Kylie Pace <kpace@cambriasolutions.com>
2017-12-19 10:23:23 -0800

    added names history test. names history container and presentions need to be refactored so test can access previous-names form from the page presentation

--- test/client/presentations/apply/names-history-test.js
+++ test/client/presentations/apply/names-history-test.js
@@ -1,1 +1,89 @@
-// select "Yes" to see form for previously used name
+'use strict';
+
+import assert                   from 'assert';
+
+import 'jsdom-global/register';
+import React                    from 'react';
+import { createMockStore }      from 'redux-test-utils';
+import { Provider }             from "react-redux";
+import { render }               from 'enzyme';
+import { spy }                  from 'sinon';
+import { MemoryRouter }         from 'react-router-dom'
+import * as dataPresent         from '../../../../client/helpers/data-present';
+
+import NamesHistoryPage         from '../../../../client/presentations/apply/used-previous-names.jsx';
+
+describe('NamesHistoryPage', function() {
+  let store = {
+    ui: {}
+  };
+
+  const Wrapper = (props) => {
+    return(
+      <Provider store={createMockStore(store)}>
+        <MemoryRouter>
+          {props.children}
+        </MemoryRouter>
+      </Provider>
+    );
+  };
+
+  describe('when it renders initially', function() {
+    let props;
+    
+    beforeEach(function() {
+      let namesHistory = {
+        hasUsedPreviousNames: '',
+        previousNames: ''
+      }
+
+      let continueDisabled = !(dataPresent.hasPreviousNames(namesHistory))
+      let onChange = spy();
+
+      props = {
+        namesHistory,
+        continueDisabled,
+        onChange
+      }
+    });
+    
+    it('shows the form asking if you have ever had a different name', function() {
+      let component = render(
+        <Wrapper>
+          <NamesHistoryPage {...props} />
+        </Wrapper>
+      );
+      assert.ok(component.find('label[for="hasUsedPreviousNamesNo"]').length, 'No button missing');
+      assert.ok(component.find('label[for="hasUsedPreviousNamesYes"]').length, 'Yes button missing');
+      assert.ok(component.find('.previous-names').length, 'form missing');
+    });
+
+    it('next button is disabled', function() {
+      let component = render(
+        <Wrapper>
+          <NamesHistoryPage {...props} />
+        </Wrapper>
+      );
+      assert.ok(component.find('.arrow-button .forward disabled'));
+    });
+
+    it('selecting No makes next button no longer disabled', function() {
+      props.namesHistory.hasPreviousNames = 'No';
+      props.continueDisabled  =   !(dataPresent.hasPreviousNames(props.namesHistory));
+
+      let component = render(
+        <Wrapper>
+          <NamesHistoryPage {...props} />
+        </Wrapper>
+      );
+
+      assert.equal(component.find('.arrow-button .forward disabled'), false);
+      assert.ok(component.find('.arrow-button forward'));
+    });
+
+    //TODO add test to check that selecting Yes makes enter-previous-names form render
+
+  });
+
+});
+
