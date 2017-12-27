53365b10643e82588cc59c517e98292e568b0674
Kylie Pace <kpace@cambriasolutions.com>
2017-12-27 11:18:28 -0800

    added csurf library to create and pass token to client and modified create application fetch function to add token to header when api is called

--- server/models/db/create-application.js
+++ server/models/db/create-application.js
@@ -63,6 +63,24 @@ module.exports = function createApplication(data) {
     .then((cards) => {
       returnedData.cards = cards;
       let card_options = data.card_options;
+      let _card_options_ = [ ];
+      let _card_id;
+
+      cards.forEach( card => {
+        card_options.forEach( option => {
+          if(card.type === option.type) {
+            _card_options_.push({
+              card_id:           card.id,
+              option_type:       option.option_type,
+              option_value:      option.option_value
+            });
+          }
+        });
+        if(card.type === 'DL') {
+          _card_id = card.id;
+        }
+      });
+      data.card_options = _card_options_;
 
       cardOptionsParser.cardOptions(cards, card_options, data);
       return insert('card_options');
