61c3ac23d878b3ffb44c381f5358b90600f77d3e
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2018-01-03 17:06:30 -0800

    Gets the header into a React portal, but alas failing tests

--- client/presentations/section-header.jsx
+++ client/presentations/section-header.jsx
@@ -1,26 +1,28 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { hasValue } from '../helpers/data/validations';
 
 const NumberPrefix = (props) => {
   if (!hasValue(props.number)) { return null; }
-  return <span>{props.number} &raquo; </span>;
+  return <div className='unit number'>{props.number}</div>
 };
 
 const SectionHeader = (props) => {
   if (!hasValue(props.name)) { return null; }
 
   return (
-    <div className='section-header row'>
-      <h3>
-        <NumberPrefix number={props.number} />
-        {props.name}
-      </h3>
-      <hr />
+    <div className='inner'>
+      <NumberPrefix number={props.number} />
+      <div className='unit name'>{props.name || '&nbsp;'}</div>
     </div>
   );
 };
 
-export default SectionHeader;
+const Header = (props) => {
+  return ReactDOM.createPortal(<SectionHeader {...props}/>, document.getElementById('section-header'));
+}
+
+export default Header;
