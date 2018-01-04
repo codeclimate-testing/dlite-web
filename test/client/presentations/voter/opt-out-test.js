3eac0481560a15ce478790d6f259ca6c025e4349
Teresa Shen <teresa.shen@dmv.ca.gov>
2018-01-04 09:17:37 -0800

    Opt out unit test

--- test/client/presentations/voter/opt-out-test.js
+++ test/client/presentations/voter/opt-out-test.js
@@ -10,27 +10,24 @@ import { spy }                  from 'sinon';
 
 import * as dataPresent         from '../../../../client/helpers/data-present';
 import OptOutPage               from '../../../../client/presentations/voter-registration/opt-out/opt-out-form.jsx';
+import store                    from '../../support/page-store';
 
 describe('OptOutPage', function() {
-  let store = {
-    ui: {}
-  };
-
   const Wrapper = wrapperGenerator(store);
-
+  
+  describe('when it renders initially', function() {
   let props;
 
   beforeEach(function() {
+    let optOut = '';
+    let continueDisabled = true;
+    let onChange = spy();
     let dateOfBirth = {
       month: '',
       day: '',
       year: ''
     };
-    let optOut = '';
-    let continueDisabled = true;
-
-    let onChange = spy();
-
+ 
     props = {
       dateOfBirth,
       optOut,
@@ -39,16 +36,16 @@ describe('OptOutPage', function() {
     }
   });
 
-  // it('shows the form asking if which best describes users voting registration status', function() {
-  //   let component = render(
-  //     <Wrapper>
-  //       <OptOutPage  {...props} />
-  //     </Wrapper>
-  //   );
-  //   assert.ok(component.find('.opt-out-form').length, 'form missing');
-  //   assert.ok(component.find('label[for="optOut-new"]').length, 'new voter button missing');
-  //   assert.ok(component.find('label[for="optOut-existing"]').length, 'existing voter button missing');
-  //   assert.ok(component.find('label[for="optOut-opt-out"]').length, 'Opt out button missing');
-  // });
-
+  it('shows the form asking if which best describes users voting registration status', function() {
+    let component = render(
+      <Wrapper>
+        <OptOutPage  {...props} />
+      </Wrapper>
+    );
+    assert.ok(component.find('.opt-out-form').length, 'form missing');
+    assert.ok(component.find('label[for="optOut-new"]').length, 'new voter button missing');
+    assert.ok(component.find('label[for="optOut-existing"]').length, 'existing voter button missing');
+    assert.ok(component.find('label[for="optOut-opt-out"]').length, 'Opt out button missing');
+  });
+});
 });
