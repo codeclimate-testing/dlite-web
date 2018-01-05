89a91e4a0fcb4ea7154f8faa43e0239d886e308e
Kylie Pace <kpace@cambriasolutions.com>
2018-01-05 14:22:58 -0800

    removed commented out console logs

--- server/config/csrf.js
+++ server/config/csrf.js
@@ -3,8 +3,6 @@ const csrf          = require('csurf');
 const cookieParser  = require('cookie-parser');
 
 function check(req, res, next) {
-  // console.log(req)
-  // send the cookie secret and csrfToken. Because httpOnly is true, the client JS can't access the csrfToken
   res.cookie('csrfToken', req.csrfToken ? req.csrfToken() : null, { sameSite: 'Strict', httpOnly: true}); 
   next();
 };
