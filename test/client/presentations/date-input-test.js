11c74240452671860399411b18eae11b8328f578
Kylie Pace <kpace@cambriasolutions.com>
2018-01-17 09:08:40 -0800

    separated unit tests into one it statement per assert statement in error checks

--- test/client/presentations/date-input-test.js
+++ test/client/presentations/date-input-test.js
@@ -10,8 +10,10 @@ import { spy }    from 'sinon';
 import DateInput  from '../../../client/presentations/date-input.jsx';
 
 describe('DateInput', function() {
-  let props;
+  let props, component;
+
   beforeEach(function() {
+
     props = {
       description: 'test',
       identifier: 'testDate',
@@ -26,79 +28,71 @@ describe('DateInput', function() {
         year: spy()
       }
     };
+    component = render(
+      <DateInput {...props} />
+    );
+
   });
 
   it('passes a string to a bold label above the number inputs', function() {
-    let component = render(
-      <DateInput {...props} />
-    );
     assert.ok(component.find('label:not(".normal")').length, 'bold label not found');
   });
 
   it('has normal-weight secondary labels for MM, DD, and YYYY', function() {
-    let component = render(
-      <DateInput {...props}/>
-    );
     assert.ok(component.find('label.normal').text().includes('MM'), 'normal MM label not found');
     assert.ok(component.find('label.normal').text().includes('DD'), 'normal DD label not found');
     assert.ok(component.find('label.normal').text().includes('YYYY'), 'normal YYYY label not found');
   });
 
-  describe('error states', function() {
-    it('when there is an error with only one of the date inputs, it will have additional text and the bold label and the input will get the error class', function() {
+  describe('when there is an error with only one of the date inputs', function() {
+
+    beforeEach(function() {
       props.validations = {
         month: () => '',
         day: () => '',
         year: () => 'error' 
       };
-      let component = render(
+      component = render(
         <DateInput {...props} />
       ); 
-      assert.equal(
-        component.find('.error').length,
-        3,
-        'error classes missing'
-      );
+    });
 
-      assert(
-        component.find('label:not(".normal").error').length,
-        'bold label does not have an error class'
-      );
-
-      assert.equal(
-        component.find('input.error').length,
-        1,
-        'input does not have an error class or too many inputs have error class'
-      );
-
+    it('it will have additional text', function() {
       assert(
         component.find('.additional-label.error').length,
         'additional label does not have an error class'
       );
     });
 
-    it('only one error icon appears', function() {
-      props.validations = {
-        month: () => '',
-        day: () => '',
-        year: () => 'error'
-      };
-      let component = render(
-        <DateInput {...props} />
+    it('will have 3 elements with the error class', function() {
+      assert.equal(
+        component.find('.error').length,
+        3,
+        'error classes missing'
       );
+    });
+
+    it('will have one error icon', function() {
       assert.equal(component.find('.error-icon').length, 1, 'incorrect number of error icons');
     });
 
+    it('will add error class to bold label', function() {
+      assert(
+        component.find('label:not(".normal").error').length,
+        'bold label does not have an error class'
+      );
+    });
+
+    it('will add error class to one input', function() {
+      assert.equal(
+        component.find('input.error').length,
+        1,
+        'input does not have an error class or too many inputs have error class'
+      );
+    });
+
     it('does not pass the error class to the labelClass variable on the secondary label', function(){
-      props.validations = {
-        month: () => 'error',
-        day: () => 'error',
-        year: () => 'error' 
-      };
-      let component = render(
-        <DateInput {...props} />
-      ); 
-      assert(component.find('label[for="month"].normal:not(".error")').length, 'secondary MM label has error class');
+      assert(component.find('label[for="year"].normal:not(".error")').length, 'secondary YYYY label has error class');
     });
   });
 });
--- test/client/presentations/number-input-test.js
+++ test/client/presentations/number-input-test.js
@@ -10,7 +10,8 @@ import { spy }    from 'sinon';
 import NumberInput  from '../../../client/presentations/number-input.jsx';
 
 describe('NumberInput not used in DateInput', function() {
-  let props;
+  let props, component;
+
   beforeEach(function() {
     props = {
       description: 'test',
@@ -18,45 +19,53 @@ describe('NumberInput not used in DateInput', function() {
       value: '',
       errorMessage: ''
     };
+
+    component = render(
+      <NumberInput {...props} />
+    );
+
   });
 
   it('passes a string to a bold label above the number inputs', function() {
-    let component = render(
-      <NumberInput {...props} />
-    );
     assert.ok(component.find('label:not(".normal")').length, 'bold label not found');
     assert.equal(component.find('label.normal').length, 0, 'secondary labels found');
   });
 
   describe('error states', function() {
-    it('when there is an error, it will have additional text and the bold label and the input will get the error class and there will be an error icon', function() {
-      props.errorMessage = 'error';
 
-      let component = render(
+    beforeEach(function() {
+      props.errorMessage = 'error';
+      component = render(
         <NumberInput {...props} />
       ); 
-      assert.equal(
-        component.find('.error').length,
-        3,
-        'error classes missing'
-      );
+    });
 
+    it('will have 3 elements with the error class', function() {
+      assert.equal(component.find('.error').length, 3, 'error classes missing');
+    });
+
+    it('will have an error icon', function() {
       assert(component.find('.error-icon').length, 'error icon missing');
+    });
 
+    it('will have additional text', function() {
+      assert(
+        component.find('.additional-label.error').length,
+        'additional label does not have an error class'
+      );
+    });
+
+    it('will add error class to bold label', function(){
       assert(
         component.find('label:not(".normal").error').length,
         'bold label does not have an error class'
       );
+    });
 
+    it('will add error class to the input', function() {
       assert.equal(
-        component.find('input.error').length,
-        1,
+        component.find('input.error').length, 1,
         'input does not have an error class'
-      );
-
-      assert(
-        component.find('.additional-label.error').length,
-        'additional label does not have an error class'
       );
     });
   });
