45f6cae8edc4aec63f573622b3f157077d680275
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-02-14 14:50:42 -0800

    Fix typos in emoji.json file

--- client/i18n/index.js
+++ client/i18n/index.js
@@ -3,6 +3,6 @@
 // NOTE: this is a placeholder for the language switching logic that will exist in the future!
 // Use this instead of going to the json directly.
 
-import * as translations from './emoji.json';
+import * as translations from './en.json';
 
 export default translations;
