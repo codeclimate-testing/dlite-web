020326ec81dc404f05da54169c5f1a69691eb681
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2018-03-05 13:09:09 -0800

    ID/DL welcome page has same styling as commercial

--- client/presentations/get-started/welcome-page.jsx
+++ client/presentations/get-started/welcome-page.jsx
@@ -3,6 +3,7 @@
 import React            from 'react';
 import Page             from '../../containers/page.jsx';
 import LinkButton       from '../link-button.jsx';
+import MessageBox       from '../message-box.jsx';
 
 const Welcome = (props) => {
   const linkAddress = '/my-basics/legal-name';
@@ -11,19 +12,39 @@ const Welcome = (props) => {
       {...props}
       sectionKey='intro'
     >
-      <div className='welcome-page'>
+      <div className='welcome-page what-you-can-do-page'>
         <h2 className='question'>Welcome!</h2>
         <p>On this website, you can:</p>
-        <ul className='bullet-list'>
-          <li>Apply for a driver license or ID card.</li>
-          <li>Renew or replace your card.</li>
-          <li>Make a change or correction to your card</li>
+        <ul>
+          <li className='translation-missing'>
+            <div className='unit icon card new'/>
+            <b>Apply</b> for a driver license or ID card.
+          </li>
+          <li className='translation-missing'>
+            <div className='unit icon card renew'/>
+            <b>Renew or replace</b> your card.
+          </li>
+          <li className='translation-missing'>
+            <div className='unit icon card change'/>
+            <b>Correct or update</b> your card.
+          </li>
         </ul>
-        <p>In order to complete this form from home, you will need to create an account. We'll walk you through the process.</p>
-        <p>If you are in a confidential address program or in Safe at Home, please do not use this form. Make an <a target="_blank" href="https://www.dmv.ca.gov/portal/dmv/dmv/onlinesvcs/appointment">appointment</a> and visit your local DMV office.</p>
+        <p>
+          In order to complete this form from home, you will need to create an account.
+          We'll walk you through the process.
+        </p>
+
+        <MessageBox className='info'>
+          <p>
+            If you are in a confidential address program or in Safe at Home,
+            please do not use this form.
+            Make an <a target="_blank" href="https://www.dmv.ca.gov/portal/dmv/dmv/onlinesvcs/appointment">appointment</a> 
+            and visit your local DMV office.
+          </p>
+        </MessageBox>
+
         <div className='navigation-buttons row'>
           <hr />
-
           <div className='shadow-container unit-right' >
             <LinkButton
               to={linkAddress}
