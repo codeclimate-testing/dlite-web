12f078fb6a48831c36543ef757cce954ba67bbb3
Kylie Pace <kpace@cambriasolutions.com>
2018-03-06 10:38:16 -0800

    used existing translations

--- client/presentations/cdl/medical-history-page.jsx
+++ client/presentations/cdl/medical-history-page.jsx
@@ -6,17 +6,20 @@ import Page                 from '../../containers/page.jsx';
 import MedicalCondition     from '../my-history/medical-history/medical-condition-info.jsx';
 import EnterMedicalInfo     from '../my-history/medical-history/enter-medical-info.jsx';
 import { hasMedical }       from '../../helpers/data/my-history';
+import translations         from '../../i18n';
+import Translate            from '../../i18n/translate-tag.jsx';
 
 const MedicalHistoryPage = (props) => {
+  let locale = props.locale;
   return (
     <Page
       {...props}
       sectionKey='myHistory'
     >
       <form onSubmit    = {props.onSubmit} className='medical-history-form'>
-        <h3>Please review the information below, then truthfully answer the question at the end of this page.</h3>
-        <h4>The following conditions that may affect your ability to operate a motor vehicle safely include, but are not limited to:</h4>
-        <ul className='medical-history bullet-list'>
+        <h3 className='translation-missing'>Please review the information below, then truthfully answer the question at the end of this page.</h3>
+        <h4 className='translation-missing'>The following conditions that may affect your ability to operate a motor vehicle safely include, but are not limited to:</h4>
+        <ul className='medical-history bullet-list translation-missing'>
           <li>Loss of consciousness</li>
           <li>Episode of marked confusion caused by
           any condition which may bring about
@@ -33,7 +36,8 @@ const MedicalHistoryPage = (props) => {
            <li>Alcohol, drug abuse, or related health problems.</li>
 
         </ul>
-        <h2 className='question'>Within the last three years, have you had or experienced any medical conditions that could affect your ability to safely operate a motor vehicle?</h2>
+
+        <h2 className='question translation-missing'>Within the last three years, have you had or experienced any medical conditions that could affect your ability to safely operate a motor vehicle?</h2>
         <MedicalCondition {...props}
           selectedValue = {props.medicalHistory.hasMedicalCondition}
         />
