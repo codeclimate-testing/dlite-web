62ecfbaa9d78aac0d2b3eb421fdb5ecf2f6aa42a
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-03-09 09:27:32 -0800

    Clean up error handling for other licenses page

--- client/helpers/validations/other-state-license-rules.js
+++ client/helpers/validations/other-state-license-rules.js
@@ -1,12 +1,18 @@
 'use strict';
 
 import selectionValidator from './selection-validator';
+import { hasValue }   from '../data/validations';
+import translations   from '../../i18n';
 
 let tenYearHistory = (props) => {
-  if(props.hasNonCALicense !== 'Yes') {
-    return [];
+  if(props.hasNonCALicense !== 'Yes') { return []; }
+
+  let locale = props.locale;
+  let errors = [];
+  if(!hasValue(props.tenYearHistory)) {
+    errors = [translations[locale].errorMessages.selectionMissing]
   }
-  return selectionValidator("selectionMissing', 'tenYearHistory")
+  return errors;
 };
 
 export default {
