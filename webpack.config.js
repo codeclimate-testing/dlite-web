f4b0293825aa6df381313345ddc0227449379ca6
Randeep Singh <logicgraphy@gmail.com>
2018-04-09 10:22:18 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ let config = {
   plugins: [
     new ExtractTextPlugin('app.css'),
     new webpack.DefinePlugin({
-      APP_ENV: JSON.stringify('production')
+      APP_ENV: JSON.stringify('stage')
     }),
     new webpack.optimize.UglifyJsPlugin(),
     new CompressionPlugin({
