68d69c9ddefe3635ab66aa6059c1a80e69a24e68
Kylie <kyliepace@gmail.com>
2018-04-21 19:09:54 -0700

    removed checkAuthentication controller from logout route (#697)
    * removed checkAuthentication controller from logout route
    
    * incorporate teresas button css changes

--- server/controllers/logout.js
+++ server/controllers/logout.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const logout = (req, res) => {
-  console.log(`IP ${req.ip} logged out at ${new Date()}. TST app: ${process.env.TST_ENV}, ADA app: ${process.env.APA_TST}`);
-  let appName = req.cookies.appName;
-  req.logout();
-  req.session.destroy();
+  console.log('LOGOUT')
+
+  if (req.session) {
+    req.logout();
+    req.session.destroy();
+  }
+
   res.clearCookie('appName');
   //res.clearCookie('dlite-web-session');
 
